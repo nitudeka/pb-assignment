4a887509bc40bae0c5f15dde83a03efbd3e89546
nitudeka <iamnitudeka@gmail.com>
2023-07-26 01:42:01 +0530

    added cards model

---
+++ backend/src/models/cards.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  cardNumber: { type: String, required: true },
+  expiryDate: { type: String, required: true },
+  cvv: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+const CardModel = mongoose.model("Card", cardSchema);
+
+module.exports = CardModel;
