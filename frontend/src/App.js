720f3862b20ff64dbb7ca3bee2724b5de7825a1e
nitudeka <iamnitudeka@gmail.com>
2023-07-25 13:25:31 +0530

    removed canvas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import Canvas from "./components/Canvas";
 
 function App() {
-  return (
-    <div className="container">
-      <Canvas />
-    </div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
---
+++ frontend/src/components/CardForm/index.js
@@ -0,0 +1,7 @@
+import { Formik, Field, Form } from "formik";
+
+const CardForm = () => {
+  return;
+};
+
+export default CardForm;
