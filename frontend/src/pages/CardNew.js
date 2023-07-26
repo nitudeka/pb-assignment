989a96070c0e6a1232db1062ba023b1b2f413722
nitudeka <iamnitudeka@gmail.com>
2023-07-26 13:31:36 +0530

    added back button on new card page

--- frontend/src/pages/CardNew.js
+++ frontend/src/pages/CardNew.js
@@ -3,8 +3,10 @@ import ImgCard from "../assets/png/credit-card.png";
 import ImgCalendar from "../assets/png/calendar.png";
 import ImgI from "../assets/png/i.png";
 import ImgUser from "../assets/png/id-card.png";
+import ImgBack from "../assets/png/back-button.png";
 import Button from "../components/Button";
 import useNewCard from "../hooks/useNewCard";
+import { useNavigate } from "react-router-dom";
 
 const CardNew = () => {
   const {
@@ -19,57 +21,68 @@ const CardNew = () => {
     onSubmit,
     errors,
   } = useNewCard();
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/");
+  };
 
   return (
-    <div className="w-96 shadow-md p-5 rounded-xl bg-white">
-      <h2 className="font-bold text-gray-700 border-b border-gray-200 pb-3">
-        Add new card
-      </h2>
-      <form onSubmit={onSubmit}>
-        <div className="mb-4">
-          <Input
-            value={cardNumber}
-            onChange={handleNumChange}
-            label="Card number"
-            placeholder="0000 0000 0000 0000"
-            icon={ImgCard}
-            name="cardNumber"
-            error={errors.cardNumber}
-          />
-          <div className="flex">
+    <div>
+      <div onClick={goBack} className="flex items-center mb-4 cursor-pointer">
+        <img alt="back" src={ImgBack} className="h-6" />
+        <p className="ml-2 text-lg">Go back</p>
+      </div>
+      <div className="w-96 shadow-md p-5 rounded-xl bg-white">
+        <h2 className="font-bold text-gray-700 border-b border-gray-200 pb-3">
+          Add new card
+        </h2>
+        <form onSubmit={onSubmit}>
+          <div className="mb-4">
             <Input
-              value={expiryDate}
-              onChange={handleExpiry}
-              name="expiryDate"
-              label="Expiry date"
-              placeholder="MM / YY"
-              icon={ImgCalendar}
-              error={errors.expiryDate}
+              value={cardNumber}
+              onChange={handleNumChange}
+              label="Card number"
+              placeholder="0000 0000 0000 0000"
+              icon={ImgCard}
+              name="cardNumber"
+              error={errors.cardNumber}
             />
-            <div className="p-2" />
+            <div className="flex">
+              <Input
+                value={expiryDate}
+                onChange={handleExpiry}
+                name="expiryDate"
+                label="Expiry date"
+                placeholder="MM / YY"
+                icon={ImgCalendar}
+                error={errors.expiryDate}
+              />
+              <div className="p-2" />
+              <Input
+                value={cvv}
+                onChange={handleCvv}
+                name="cvv"
+                label="CVV"
+                placeholder="&#8226; &#8226; &#8226;"
+                icon={ImgI}
+                type="password"
+                error={errors.cvv}
+              />
+            </div>
             <Input
-              value={cvv}
-              onChange={handleCvv}
-              name="cvv"
-              label="CVV"
-              placeholder="&#8226; &#8226; &#8226;"
-              icon={ImgI}
-              type="password"
-              error={errors.cvv}
+              value={name}
+              onChange={handleName}
+              name="name"
+              label="Cardholder name"
+              placeholder="Enter cardholder's full name"
+              icon={ImgUser}
+              error={errors.name}
             />
           </div>
-          <Input
-            value={name}
-            onChange={handleName}
-            name="name"
-            label="Cardholder name"
-            placeholder="Enter cardholder's full name"
-            icon={ImgUser}
-            error={errors.name}
-          />
-        </div>
-        <Button type="submit" text="Add card" />
-      </form>
+          <Button type="submit" text="Add card" />
+        </form>
+      </div>
     </div>
   );
 };
