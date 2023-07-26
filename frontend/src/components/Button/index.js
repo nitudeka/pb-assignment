d7457de1745032195f14642e05ac18adbda660e2
nitudeka <iamnitudeka@gmail.com>
2023-07-26 13:30:57 +0530

    changed button styles

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -1,10 +1,11 @@
 const Button = (props) => {
-  const { text, type } = props;
+  const { text, type, onClick } = props;
 
   return (
     <button
       type={type || "button"}
-      className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+      className="w-full bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-lg"
+      onClick={onClick}
     >
       {text}
     </button>
