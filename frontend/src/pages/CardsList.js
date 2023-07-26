0ffbc6ea185393a8eda71bc57550c871c505e437
nitudeka <iamnitudeka@gmail.com>
2023-07-26 13:31:26 +0530

    created CardList page

--- frontend/src/pages/CardsList.js
+++ frontend/src/pages/CardsList.js
@@ -1,5 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Card from "../components/Card";
+import useCardList from "../hooks/useCardList";
+
 const CardsList = () => {
-  return <div>list card</div>;
+  const { cards, loading } = useCardList();
+  const navigate = useNavigate();
+
+  const onAddNewCard = () => {
+    navigate("/new");
+  };
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="w-96">
+      <div className="mb-4">
+        {Boolean(cards.length) &&
+          cards.map((card) => <Card key={card.cardNumber} {...card} />)}
+        {!loading && !cards.length && (
+          <div className="w-full shadow rounded-lg bg-white p-4">
+            <p className="text-center font-bold text-xl">No cards saved yet</p>
+          </div>
+        )}
+      </div>
+      <Button text="Add new card" onClick={onAddNewCard} />
+    </div>
+  );
 };
 
 export default CardsList;
